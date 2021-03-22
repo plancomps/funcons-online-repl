204249f82448008ec6c4866810b43bc020602b81
DF <leegbestand@gmail.com>
2021-03-22 22:51:33 +0100

    Add fit in container and ubuntu styling.

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 import socket from "./socket";
+import { FitAddon } from 'xterm-addon-fit';
 
 socket.connect();
-var term = new Terminal();
+var term = new Terminal({
+    theme: {
+        background: '#300A24',
+        foreground: '#ffffff'
+    }
+});
+
+const fitAddon = new FitAddon();
 
 socket.on("connect_error", (err) => { term.write("\n\r--- Server not responding...."); });
 
@@ -16,4 +24,5 @@ term.onKey((ev) => {
     //var enc = new TextEncoder(); // TODO: encode for utf-8?
     socket.emit("command", ev.key);
 });
-
+terminal.loadAddon(fitAddon);
+fitAddon.fit();
